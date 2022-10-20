f537bd7460e0f0350f1b956962a74afcb24d4f21
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-10-20 20:01:13 +0200

    added mobile/pc view

---
+++ app.js
@@ -0,0 +1,20 @@
+const email = document.querySelectorAll('input')
+const notify = document.querySelector('button')
+const error = document.querySelectro('err')
+
+
+
+
+function verifyEmail() {
+    if 
+
+}
+
+
+
+
+
+
+
+email.addEventListener('input', ......)
+notify.addEventListener('click', verifyEmail)
