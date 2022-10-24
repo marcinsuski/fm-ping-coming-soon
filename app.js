c74f8e61d9644c04dfd1541642b4cf69aa496605
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-10-24 12:27:59 +0200

    css + js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const email = document.querySelectorAll('input')
-const notify = document.querySelector('button')
-const error = document.querySelectro('err')
+const email = document.getElementById('email')
+const notify = document.querySelector('.button')
+const error = document.querySelector('#err')
+const border = document.querySelector('.input')
 
 
 
-
-function verifyEmail() {
-    if 
-
+function validateEmail() {
+    if (email.value.includes('@')) {
+        email.style.border = 'grey';
+        error.style.color = 'blue';
+        error.innerHTML = 'Email is correct';
+    } else {
+        email.style.border = "1px solid red";
+        error.style.color = 'red'
+        error.innerHTML = 'Please provide a valid email address';
+    }
 }
 
 
 
-
-
-
-
-email.addEventListener('input', ......)
-notify.addEventListener('click', verifyEmail)
+notify.addEventListener('click', validateEmail);
